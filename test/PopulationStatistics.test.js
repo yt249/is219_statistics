9feb5f9235900c69aa7cb3d2cb417c8f7f90f1f3
vartika99 <54948113+vartika99@users.noreply.github.com>
2021-03-26 21:28:25 -0400

    added population stats and tests for it

---
+++ test/PopulationStatistics.test.js
@@ -0,0 +1,69 @@
+const expect = require("expect");
+const RandomGenerator = require('../src/Statistics/RandomGenerator');
+const Descriptive = require('../src/Statistics/DescriptiveStatistics');
+const PopulationSampling = require('../src/Statistics/PopulationSampling');
+
+seed = 10;
+
+test('simple random sample', () => {
+    let size = 10;
+    let randList = RandomGenerator.SeededRandomListInteger(10,10, 100, size);
+    let sampleSize = RandomGenerator.SeededRandomInteger(1, 10, (size));
+    let list1 = PopulationSampling.simpleRandomSample(randList, sampleSize, 10 );
+    let list2 = PopulationSampling.simpleRandomSample(randList, sampleSize, 10);
+
+    expect(list1).toHaveLength(Math.ceil(sampleSize));
+    expect(list1).toEqual(list2);
+});
+
+test('systematic random sample', () => {
+   let size = 10;
+   let randList = RandomGenerator.SeededRandomListInteger(seed, 10, 100, size);
+   let sampleSize = RandomGenerator.SeededRandomInteger(seed, 1, size - 1);
+   let sampleList = PopulationSampling.systematicSample(randList, sampleSize);
+
+   expect(sampleList).toHaveLength(sampleSize);
+});
+
+test('z score from confidence', () => {
+    expect(PopulationSampling.getZScoreFromConfidence(85).toEqual(1.44));
+    expect(PopulationSampling.getZScoreFromConfidence(95).toEqual(1.96));
+});
+
+test('margin of error', () => {
+    let size = 10;
+    let sampleList = RandomGenerator.SeededRandomListInteger(seed, -100, 100, size);
+    let confidence = Math.floor(RandomGenerator.SeededRandomInteger(seed, 50, 95) / 5) * 5;
+    let marginOfError = PopulationSampling.marginOfError(sampleList, confidence);
+
+    expect(marginOfError).toBeGreaterThan(0);
+});
+
+test('confidence interval', () => {
+    let size = 10;
+    let sampleList = RandomGenerator.SeededRandomListInteger(seed, -100, 100, size);
+    let confidence = Math.floor(RandomGenerator.SeededRandomInteger(seed, 50, 95) / 5) *5;
+    let confidenceInterval = PopulationSampling.confidenceInterval(sampleList, confidence);
+
+    let mean = Descriptive.Mean(sampleList);
+    expect(confidenceInterval).toHaveLength(2);
+    expect(confidenceInterval[0]).toBeLessThan(mean);
+    expect(confidenceInterval[1]).toBeGreaterThan(mean);
+});
+
+test('cochran sample size', () => {
+    expect(PopulationSampling.cochran(95, 5, 0.5, 1000)).toEqual(278);
+});
+
+test('sample size with no std dev', () => {
+    expect(PopulationSampling.sampleSizeNoStdDev(95, 10, 0.5)).toBeGreaterThan(0);
+});
+
+test('sample size with std dev', () => {
+    let size = 10;
+    let sampleList = RandomGenerator.SeededRandomListInteger(100, -100, 100, size);
+    let confidence = Math.floor(RandomGenerator.SeededRandomInteger(100, 50, 95) / 5) * 5;
+    let stdDev = Descriptive.StandardDeviation(sampleList);
+
+    expect(PopulationSampling.sampleSizeStdDev(confidence, 10, stdDev)).toBeGreaterThan(0);
+});
